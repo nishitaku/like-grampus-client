41e7bd1580fdfa1d16bdbf002bbb2435fe002e93
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-04-24 11:45:10 +0900

    ランキングページを仮実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,68 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
+  <v-container>
+    <v-row justify="center">
+      <div class="text-center">{{ username }}さんの順位 {{ rank }}位</div>
+    </v-row>
+    <v-row justify="center">
+      <v-card width="80%">
+        <v-list two-line disabled>
+          <v-list-item-group>
+            <template v-for="(item, index) in items">
+              <v-list-item :key="item.uploadImgUrl">
+                <v-list-item-icon>
+                  <v-icon>mdi-star</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title
+                    v-text="`${item.displayName} さん`"
+                  ></v-list-item-title>
+                  <v-list-item-subtitle
+                    v-text="item.score"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-avatar>
+                  <v-img :src="item.uploadImgUrl"></v-img>
+                </v-list-item-avatar>
+              </v-list-item>
+              <v-divider
+                v-if="index + 1 < items.length"
+                :key="item.uploadImgUrl"
+              ></v-divider>
+            </template>
+          </v-list-item-group>
+        </v-list>
       </v-card>
-    </v-flex>
-  </v-layout>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      username: 'テスト',
+      rank: '5',
+      items: [
+        {
+          displayName: 'AAA',
+          score: 0.9,
+          uploadImgUrl:
+            'https://upload-images.s3.jp-tok.cloud-object-storage.appdomain.cloud/f33e07cade6721fd86c7af1676d2a6.jpg'
+        },
+        {
+          displayName: 'AAA',
+          score: 0.9,
+          uploadImgUrl:
+            'https://upload-images.s3.jp-tok.cloud-object-storage.appdomain.cloud/f33e07cade6721fd86c7af1676d2a6.jpg'
+        },
+        {
+          displayName: 'AAA',
+          score: 0.9,
+          uploadImgUrl:
+            'https://upload-images.s3.jp-tok.cloud-object-storage.appdomain.cloud/f33e07cade6721fd86c7af1676d2a6.jpg'
+        }
+      ]
+    }
   }
 }
 </script>
