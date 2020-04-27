bc266c0d7d8688a7a6c1209f34a92b64d338bf3e
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-04-27 14:50:00 +0900

    Netlify FunctionsをTypescriptに変更

---
+++ functions/api/hello.ts
@@ -0,0 +1,24 @@
+import { Handler, Context, Callback, APIGatewayEvent } from 'aws-lambda'
+
+interface HelloResponse {
+  statusCode: number
+  body: string
+}
+
+export const handler: Handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback
+) => {
+  const params = event.queryStringParameters
+  const response: HelloResponse = {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: `Hello world ${Math.floor(Math.random() * 10)}`,
+      requestId: context.awsRequestId || 'dummy',
+      params
+    })
+  }
+
+  callback(undefined, response)
+}
