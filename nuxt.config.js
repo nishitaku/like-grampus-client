8002f5f60b7d1d47e4cbe212066076dc79ed8409
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-04-27 22:26:39 +0900

    デプロイしたらaxiosがlocalhost:3000を呼び出す問題の対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: '/.netlify/functions/'
+    baseURL: '/'
   },
   /*
    ** vuetify module configuration
