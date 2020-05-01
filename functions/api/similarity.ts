c931cc894960fd889633cd8f2d14b3b358da4062
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-05-01 13:36:52 +0900

    kintoneから類似度判定結果を取得するfunctionsを追加

---
+++ functions/api/similarity.ts
@@ -0,0 +1,47 @@
+import { Handler, Context, APIGatewayEvent } from 'aws-lambda'
+import { KintoneRestAPIClient } from '@kintone/rest-api-client'
+
+interface Response {
+  statusCode: number
+  body: string
+}
+
+export const handler: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
+  console.log(`event=${JSON.stringify(event)}`)
+  console.log(`context=${JSON.stringify(context)}`)
+  let response: Response
+  if (event.httpMethod === 'GET') {
+    console.log(`GET called`)
+    // const params = await event.queryStringParameters
+    const kintoneClient = createClassifiedAppClient()
+    const result = await kintoneClient.record.getRecords({
+      app: process.env.KINTONE_CLASSIFIED_APP_ID || ''
+    })
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(result.records)
+    }
+  } else {
+    response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        msg: 'Bad Request'
+      })
+    }
+  }
+
+  return response
+}
+
+function createClassifiedAppClient(): KintoneRestAPIClient {
+  const client = new KintoneRestAPIClient({
+    baseUrl: process.env.KINTONE_BASE_URL || '',
+    auth: {
+      apiToken: process.env.KINTONE_CLASSIFIED_APP_API_TOKEN || ''
+    }
+  })
+  return client
+}
